63c1cb477842e5d9d5ec09d920e618d94a6a9260
TW-ITBear <twinstar0614@gmail.com>
2016-04-06 17:26:19 +0800

    first step of TDD, note that Arrange, Act, and Assert

---
+++ MathLibrary/Calculate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class Calculate
+    {
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public int Whoami(int firstNumer)
+        {
+            return firstNumer;
+        }
+
+        public int Minus(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+    }
+}
---
+++ MathLibraryTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibrary;
+
+namespace MathLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            Calculate cl = new Calculate();
+            int firstNumber = 1;
+            int secondNumber = 2;
+            int expected = 3;
+ 
+            //Act
+            int actual;
+            actual = cl.Add(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        
+        [TestMethod]
+        public void MinusTest()
+        {
+            //Arrange
+            Calculate cl = new Calculate();
+            int firstNumber = 3;
+            int secondNumber = 2;
+            int expect = 1;
+
+            //Act
+            int actual;
+            actual = cl.Minus(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
+
+
+    }
+}
